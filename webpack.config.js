859d7a3a5e528f89b75d87c99db08ac2dc4d606f
anxoret <post.lux@yandex.ru>
2019-12-09 20:10:02 +0400

    NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,21 @@
 "use strict";
 
+const NODE_ENV = process.env.NODE_ENV || "development";
+
 module.exports = {
     entry: "./home", // файл, который нужно собирать
     output: { // куда выводить   
         filename: "build.js"
     }
 
-    watch: true,
+    // watch: true,
+    watch: NODE_ENV == "development",
 
     watchOptions: {
         aggregateTimeout: 100
     },
 
     // devtool: "source-map" for production
-    devtool: "cheap-inline-module-source-map" // or eval for development
+    devtool: NODE_ENV == "development" ? "cheap-inline-module-source-map" : null
+    // or eval/cheap-inline-module-source-map for development
 };
