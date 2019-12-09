19827316d503832da112ddd47a233940316d8daa
anxoret <post.lux@yandex.ru>
2019-12-09 17:58:51 +0400

    Adding devtool in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,4 +5,13 @@ module.exports = {
     output: { // куда выводить   
         filename: "build.js"
     }
+
+    watch: true,
+
+    watchOptions: {
+        aggregateTimeout: 100
+    },
+
+    // devtool: "source-map" for production
+    devtool: "cheap-inline-module-source-map" // or eval for development
 };
